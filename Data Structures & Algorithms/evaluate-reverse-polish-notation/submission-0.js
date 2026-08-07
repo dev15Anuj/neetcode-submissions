7005d367fe8d62c9069e80e96e855d0feba7bf82
Anuj kumar <anujkumar102ak@gmail.com>
2026-08-07 22:25:25 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+
+        for (let token of tokens) {
+            if (token === "+" || token === "-" || token === "*" || token === "/") {
+                const b = stack.pop();
+                const a = stack.pop();
+
+                if (token === "+") {
+                    stack.push(a + b);
+                } else if (token === "-") {
+                    stack.push(a - b);
+                } else if (token === "*") {
+                    stack.push(a * b);
+                } else {
+                    stack.push(Math.trunc(a / b));
+                }
+            } else {
+                stack.push(Number(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
